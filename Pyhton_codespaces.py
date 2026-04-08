202cff176ef54a5461d2a80e8a046f3ea62b6925
josuepenroz <josue.penroz.maldonado@alumnos.sip.cl>
2026-04-08 16:29:39 +0000

    2_cambio

--- Pyhton_codespaces.py
+++ Pyhton_codespaces.py
@@ -1,1 +1,13 @@
-print("Hi")    
+#Herencia 
+class Usuario:
+    def __init__(self, nombre, contraseña):
+        self.nombre = nombre
+        self.contraseña = contraseña
+        self.activo = True
+    def login(self, nombre, nontraseña):
+        self.activo = True
+        return f"{self.nombre} Esta logeado" 
+    def logout(self):
+        self.activo = False
+        return f"{self.nombre} se ha deslogeado"
+class Admin
